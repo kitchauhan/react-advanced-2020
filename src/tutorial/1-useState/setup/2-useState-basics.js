c1d0320337f1b59c5a2582f82d74bb806f4a40f0
John <johnsmilga@gmail.com>
2020-12-05 08:19:13 -0800

    readme fixed

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
+// use
+// Component name must be uppercase
+// must be in the function/component body
+// cannot call condifionally
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  //console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
 
-export default UseStateBasics;
+  const [text, setText] = useState('random title')
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello world')
+    } else {
+      setText('random title')
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handleClick}>
+        Change title
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateBasics
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'Bhaskar',
+    age: 34,
+    mesaage: 'My message',
+  })
+  const changeMessage = () => {
+    setPerson({
+      ...person,
+      mesaage: 'My message 1',
+    })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.mesaage}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
+}
 
-export default UseStateObject;
+export default UseStateObject
